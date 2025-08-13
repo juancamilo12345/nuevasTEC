423c269d1a7ac518d628bdd4e3b0edd776f473d1
Juan Camilo Medina <juancamilomedinafernandez2020@gmail.com>
2025-08-13 10:58:05 -0500

    mostrar menu al cliente

---
+++ app.py
@@ -0,0 +1,20 @@
+#se agrega inventario de tienda con un diccionario
+
+inventario_tienda = {
+    "manzanas": 50,
+    "naranjas": 30,
+    "platanos": 20,
+    "fresas": 15
+    "carne": 100,
+    "pan": 80,
+    "leche": 60,
+    "huevos": 40,
+    "queso": 25
+    "arroz": 90,
+    "pasta": 70,
+    "aceite": 45
+    "azucar": 55,
+    "sal": 35,
+    "cereal": 65,
+    "jugo": 75
+}
